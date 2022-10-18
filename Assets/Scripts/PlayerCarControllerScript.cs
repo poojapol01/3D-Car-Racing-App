8ca2ec6c64b36dc3ecf5fbfd6d5ad2cb7dc437ed
poojapol01 <polpooja05@gmail.com>
2022-10-18 20:47:54 +0530

    Added Car Movememts for Front Wheels Drive

---
+++ Assets/Scripts/PlayerCarControllerScript.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCarControllerScript : MonoBehaviour
+{
+    [Header("Wheels Collider")]
+    public WheelCollider frontLeftWheelCollider;
+    public WheelCollider frontRightWheelCollider;
+    public WheelCollider rearLeftWheelCollider;
+    public WheelCollider rearRightWheelCollider;
+
+    [Header("Wheels Transform")]
+    public Transform frontLeftWheelTransform;
+    public Transform frontRightWheelTransform;
+    public Transform rearLeftWheelTransform;
+    public Transform rearRightWheelTransform;
+
+    [Header("Car Engine")]
+    public float accelerationForce = 300f;
+    public float breakingForce = 300f;
+    private float presentBreakForce = 0f;
+    private float presentAcceleration = 0f;
+
+    private void Update(){
+        MoveCar();
+    }
+    
+    private void MoveCar()
+    {
+        //Front Wheels Drive
+        frontLeftWheelCollider.motorTorque = presentAcceleration;
+        frontRightWheelCollider.motorTorque = presentAcceleration;
+        
+        presentAcceleration = accelerationForce * Input.GetAxis("Vertical");
+    }
+
+}
